6cedecb30ec058f39bc266a93ad0f88fe3a07068
Nicolas <nicolas@nicolasf.uy>
2023-05-04 00:04:25 -0300

    Initial commit

---
+++ ObligatorioDDA/src/inicio/ObligatorioDDA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inicio;
+
+import ui.TestingApp;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class ObligatorioDDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new TestingApp(null, false).setVisible(true);
+        //
+        //
+    }
+    
+}
---
+++ ObligatorioDDA/src/logica/Fachada.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Fachada {
+    
+    //instanciar los controles
+    
+}
