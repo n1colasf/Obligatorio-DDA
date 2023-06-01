4526b9daa48071e69ee09c1a3cbab18e36153f12
Nicolas <nicolas@nicolasf.uy>
2023-06-01 01:44:01 -0300

    interfaz observador y clase observable

---
+++ ObligatorioDDA/src/logica/observador/Observable.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.observador;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Observable {
+    
+    private ArrayList<Observador> observadores = new ArrayList();
+    
+    public void agregarObservador(Observador obs){
+        if(!observadores.contains(obs)){
+            observadores.add(obs);
+        }
+    }
+    public void quitarObservador(Observador obs){
+        observadores.remove(obs);
+    }
+    public void avisar(Object evento){
+       
+        ArrayList<Observador> copia = new ArrayList(observadores);
+        for(Observador obs:copia){
+            obs.actualizar(evento, this);
+        }
+    }
+}
---
+++ ObligatorioDDA/src/logica/observador/Observador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package logica.observador;
+
+/**
+ *
+ * @author Nicolas
+ */
+public interface Observador {
+    public void actualizar(Object evento,Observable origen);
+}
